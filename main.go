ead4eb76025fea578dc5b8db81f82f544e9c85a6
Peter Krantz <zyrree@gmail.com>
2022-11-11 21:11:57 +0100

    parse history file and counts unique instances

--- main.go
+++ main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
 	shell := currentShell()
-	switch shell {
-	case "bash":
-		fmt.Println("bash")
-	case "zsh":
-		fmt.Println("zsh")
-	default:
-		fmt.Println("Unknown or unsupported shell. Exiting.")
-		os.Exit(1)
-	}
+	commands := readHistFile(shell)
+	uniqueCommands := countUniqueCommands(commands)
+	fmt.Println(uniqueCommands)
 }
 
 func currentShell() string {
@@ -24,4 +19,43 @@ func currentShell() string {
 	shellSlice := strings.Split(shell, "/")
 	shell = shellSlice[len(shellSlice)-1]
 	return shell
+}
+
+func readHistFile(shell string) []string {
+	var histFile string
+
+	switch shell {
+	case "bash":
+		histFile = os.Getenv("HOME") + "/.bash_history"
+	case "zsh":
+		histFile = os.Getenv("HOME") + "/.zsh_history"
+	default:
+		fmt.Println("Unknown or unsupported shell. Exiting.")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(histFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileString := strings.Split(string(fileBytes), "\n: ")
+	for i, v := range fileString {
+		fileString[i] = strings.Split(strings.TrimSpace(v), ";")[1]
+	}
+	return fileString
+}
+
+func countUniqueCommands(commands []string) map[string]int {
+	uniqueCommands := make(map[string]int)
+	for _, v := range commands {
+		uniqueCommands[v]++
+	}
+	return uniqueCommands
 }
